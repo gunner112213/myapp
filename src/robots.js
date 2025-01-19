326b11c10b87f3b5ccaf500cf2d2a6fe4496e9dd
gunnerataiphone <gunner.hendricks@aiphone.com>
2025-01-18 21:06:07 -0800

    New front-end React App

---
+++ src/robots.js
@@ -0,0 +1,73 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'DipShit',
+        username: 'Bret',
+        email: 'testing@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'antonette@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Kenn',
+        username: 'KennBen',
+        email: 'Kenn15@hotmail.com'
+    },
+    {
+        id: 4,
+        name: 'Kenn',
+        username: 'KennBen',
+        email: 'Kenn15@hotmail.com'
+    },
+    {
+        id: 5,
+        name: 'Kenn',
+        username: 'KennBen',
+        email: 'Kenn15@hotmail.com'
+    },
+    {
+        id: 6,
+        name: 'Kenn',
+        username: 'KennBen',
+        email: 'Kenn15@hotmail.com'
+    },
+    {
+        id: 7,
+        name: 'Kenn',
+        username: 'KennBen',
+        email: 'Kenn15@hotmail.com'
+    },
+    {
+        id: 8,
+        name: 'Kenn',
+        username: 'KennBen',
+        email: 'Kenn15@hotmail.com'
+    },
+    {
+        id: 9,
+        name: 'Kenn',
+        username: 'KennBen',
+        email: 'Kenn15@hotmail.com'
+    },
+    {
+        id: 10,
+        name: 'Kenn',
+        username: 'KennBen',
+        email: 'Kenn15@hotmail.com'
+    },
+    {
+        id: 11,
+        name: 'Kenn',
+        username: 'KennBen',
+        email: 'Kenn15@hotmail.com'
+    }, {
+        id: 12,
+        name: 'Kenn',
+        username: 'KennBen',
+        email: 'Kenn15@hotmail.com'
+    }
+]
